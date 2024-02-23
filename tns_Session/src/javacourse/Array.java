bf3c77173e9f35dd9a0db35c00c84ce73d1e8dd2
MitaliPatel1 <109495852+MitaliPatel1@users.noreply.github.com>
2024-02-23 16:22:01 +0530

    Add files via upload

---
+++ tns_Session/src/javacourse/Array.java
@@ -0,0 +1,22 @@
+package javacourse;
+
+public class Array {
+
+	public static void main(Stringuplo[] args) {
+		int a[]= {1,2,3,4,5,6,7};
+		int count=0;
+		for(int i=0;i<a.length;i++) {
+			if(a[i]%2==0) {
+				count++;
+			}
+			
+			
+		}
+		System.out.println(count);
+		
+			
+		}
+
+	}
+
+
